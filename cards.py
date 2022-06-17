341712e46bc48e28e5d9ceebdfa3ea001fcfec97
tymowskyy <tymowskyy@gmail.com>
2022-06-17 17:41:18 +0200

    added CardSuit eum type

---
+++ cards.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class CardSuit(Enum):
+    HEARTS = '\u2665'
+    DIAMONDS = '\u2666'
+    CLUBS = '\u2663'
+    SPADES = '\u2660'
+
+    def __str__(self):
+        return self.value
+
+print([str(i) for i in CardSuit])
