fa309c2e25cce1eb1cfd28776b20913bdb6dcf89
tymowskyy <tymowskyy@gmail.com>
2022-06-17 20:31:23 +0200

    added CardValues Enum type

--- cards.py
+++ cards.py
@@ -9,3 +9,31 @@ class CardSuit(Enum):
 
     def __str__(self):
         return self.value
+
+
+class CardValues(Enum):
+    TWO = (1, '2')
+    THREE = (2, '3')
+    FOUR = (3, '4')
+    FIVE = (4, '5')
+    SIX = (5, '6')
+    SEVEN = (6, '7')
+    EIGHT = (7, '8')
+    NINE = (8, '9')
+    TEN = (9, '10')
+    JACK = (10, 'J')
+    QUEEN = (11, 'Q')
+    KING = (12, 'K')
+    ACE = (13, 'A')
+
+    def __str__(self):
+        return self.value[1]
+
+    def __lt__(self, other):
+        return self.value[0] < other.value[0]
+    def __le__(self, other):
+        return self.value[0] <= other.value[0]
+    def __gt__(self, other):
+        return self.value[0] > other.value[0]
+    def __ge__(self, other):
+        return self.value[0] >= other.value[0]
--- cards_tests.py
+++ cards_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from cards import CardSuit
+from cards import CardSuit, CardValues
 
 
 class TestCards(unittest.TestCase):
@@ -10,6 +10,18 @@ class TestCards(unittest.TestCase):
         self.assertEqual(str(CardSuit.CLUBS), '♣')
         self.assertEqual(str(CardSuit.SPADES), '♠')
 
+    def test_card_values_strings(self):
+        self.assertEqual(str(CardValues.QUEEN), 'Q')
+
+    def test_card_values_comparsion(self):
+        self.assertTrue(CardValues.THREE > CardValues.TWO)
+        self.assertFalse(CardValues.THREE > CardValues.THREE)
+        self.assertTrue(CardValues.THREE >= CardValues.THREE)
+        self.assertFalse(CardValues.THREE >= CardValues.KING)
+        self.assertTrue(CardValues.JACK < CardValues.ACE)
+        self.assertFalse(CardValues.KING < CardValues.THREE)
+        self.assertTrue(CardValues.KING <= CardValues.KING)
+        self.assertFalse(CardValues.ACE <= CardValues.KING)
 
 if __name__ == '__main__':
     unittest.main()
