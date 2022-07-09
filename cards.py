d75c0b6cb11ab5f9cff6f99f3a9359af0284ceb5
tymowskyy <tymowskyy@gmail.com>
2022-07-10 00:25:20 +0200

    added hand ranking

--- cards.py
+++ cards.py
@@ -101,15 +101,92 @@ class Hand:
         self.cards = cards
         self.cards_sorted = sorted(self.cards)[::-1]
 
+        self.amounts_of_card_values = dict.fromkeys(CardValue, 0)
+        for card in cards:
+            self.amounts_of_card_values[card.value] += 1
+
+        self.HAND_RANKING = [
+            ('Royal Flush', self.is_royal_flush),
+            ('Straight Flush', self.is_straight_flush),
+            ('Four of a Kind', self.is_four_of_a_kind),
+            ('Full House', self.is_full_house),
+            ('Flush', self.is_flush),
+            ('Straight', self.is_straight),
+            ('Three of Kind', self.is_three_of_kind),
+            ('Two Pair', self.is_two_pair),
+            ('One Pair', self.is_one_pair),
+            ('High Card', self.is_high_card)
+        ]
+
     def __str__(self):
         return '[' + reduce(lambda x, y: str(x) + ', ' + str(y), self.cards_sorted) + ']'
+
+    def is_royal_flush(self):
+        return self.is_straight_flush() and self.cards_sorted[0].value == CardValue.ACE
+    
+    def is_straight_flush(self):
+        return self.is_straight() and self.is_flush()
+
+    def is_four_of_a_kind(self):
+        for value, amount in self.amounts_of_card_values.items():
+            if amount == 4:
+                return True
+        return False
+
+    def is_full_house(self):
+        return self.is_three_of_kind() and self.is_one_pair()
+
+    def is_flush(self):
+        suit = self.cards[0].suit
+        for card in self.cards[1:]:
+            if card.suit != suit:
+                return False
+        return True
+
+    def is_straight(self):
+        previous_card = self.cards_sorted[0]
+        for card in self.cards_sorted[1:]:
+            if previous_card.value.value[0] != card.value.value[0] + 1:
+                return False
+            previous_card = card
+        return True
+
+    def is_three_of_kind(self):
+        for value, amount in self.amounts_of_card_values.items():
+            if amount == 3:
+                return True
+        return False
+
+    def is_two_pair(self):
+        was_pair = False
+        for value, amount in self.amounts_of_card_values.items():
+            if amount == 2:
+                if was_pair:
+                    return True
+                else:
+                    was_pair = True
+        return False
+
+    def is_one_pair(self):
+        for value, amount in self.amounts_of_card_values.items():
+            if amount == 2:
+                return True
+        return False
+
+    def is_high_card(self):
+        return True
+
+    def get_best_hand(self):
+        for hand in self.HAND_RANKING:
+            if hand[1]():
+                return hand[0]
 
 h = Hand([
     Card(CardSuit.HEARTS, CardValue.JACK),
     Card(CardSuit.SPADES, CardValue.KING),
     Card(CardSuit.HEARTS, CardValue.TWO),
     Card(CardSuit.HEARTS, CardValue.ACE),
-    Card(CardSuit.DIAMONDS, CardValue.FOUR)
+    Card(CardSuit.DIAMONDS, CardValue.ACE)
 ])
-
-print(h)
+print(h.amounts_of_card_values)
+print(h.get_best_hand())
